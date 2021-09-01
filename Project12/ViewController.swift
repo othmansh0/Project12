508384c5ceb6acfb24b2662e443377ca74953afe
Othman Shahrouri <othmanshahrouri@gmail.com>
2021-09-01 10:36:27 +0300

    Defaults notes

--- Project12/ViewController.swift
+++ Project12/ViewController.swift
@@ -11,7 +11,38 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        //Examples of writing data:
+        let defaults = UserDefaults.standard
+        defaults.set(25, forKey: "Age")
+        defaults.set(true, forKey: "UseFaceID")
+        defaults.set(CGFloat.pi, forKey: "Pi")
+        
+        defaults.setValue("Othman Shahrouri", forKey: "Name")
+        defaults.set(Date(), forKey: "LastRun")
+        
+        let array = ["hello","world"]
+        defaults.set(array, forKey: "SavedArray")
+        
+        let dict = ["Name": "Paul" ]
+        defaults.set(dict,forKey: "SavedDict")
+        
+        //Examples of reading data:
+        
+        let age = defaults.integer(forKey: "Age")
+        print(age)
+        
+        let useFaceID = defaults.bool(forKey: "UseFaceID")
+        print(useFaceID)
+        
+        //Usually used for arrays and dictionaries
+       // object(forKey:) returns Any? so you need to conditionally typecast it to your data type
+        
+        let dict2 = defaults.object(forKey: "SavedDict") as? [String:String] ?? [String:String]()
+        
+        let array2 = defaults.object(forKey: "SavedArray") as? [String] ?? [String]()
+        
+        
     }
 
 
